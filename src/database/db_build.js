4b59b3a44dbe70f8938a9cb369def58aa336cdcf
lubnaabad <lubna_mosa@hotmail.com>
2018-09-13 11:45:14 +0300

    fixed database build relates #24

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -5,7 +5,7 @@ const dbConnection = require('./db_connection');
 const sql = fs.readFileSync(path.join(__dirname, '/db_build.sql')).toString();
 
 const runbuild = cb => dbConnection.query(sql, (err, res) => {
-  if (err) { cb(err); }
+  if (err) return cb(err);
   cb(null, res);
 });
 module.exports = runbuild;
