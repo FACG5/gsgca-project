f43b3bdfbd7fb8a25409909f73dc4e1ebd775ce2
hemasali <hemo3li7@gmail.com>
2018-09-16 13:39:35 +0300

    adminHomePge and logout and validation of login relates #21 , #29

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const login = require('./login');
-
+const adminHomePage = require('./adminHomePage');
+const { authCheck } = require('./middleware');
 
 const router = express.Router();
 router.get('/admin/login', login.get);
 router.post('/admin/login', login.post);
-
-
+router.use(authCheck);
+router.get('/admin',adminHomePage.get);
+router.get('/admin/logout',adminHomePage.logout);
 module.exports = router;
