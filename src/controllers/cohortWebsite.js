442cb2ca0b0473a628e7eb3557d47f9786415d4e
lubnaabad <lubna_mosa@hotmail.com>
2018-09-18 14:39:12 +0300

    view cohort in the webSite relates #46

--- src/controllers/cohortWebsite.js
+++ src/controllers/cohortWebsite.js
@@ -1,7 +1,20 @@
+const getCohortsQuery = require('../database/queries/getCohorts');
+
 exports.get = (request, response) => {
-  response.render('cohortWebsite', {
-    cohortPage: 'active',
-    layout: 'webSite',
-    styleFile: 'cohortWebsite',
+  getCohortsQuery((err, res) => {
+    if (err) {
+      return response.render('homeWebsite', {
+        layout: 'webSite',
+        home: 'active',
+        styleFile: 'homeWebsite',
+        jsFile: 'homeWebsite',
+      });
+    }
+    return response.render('cohortWebsite', {
+      res,
+      cohortPage: 'active',
+      layout: 'webSite',
+      styleFile: 'cohortWebsite',
+    });
   });
 };
