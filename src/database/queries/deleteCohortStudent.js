e17fee2693c790306a24d77f0118b97586075e3d
lubnaabad <lubna_mosa@hotmail.com>
2018-09-18 09:56:03 +0300

    fix function delete

--- src/database/queries/deleteCohortStudent.js
+++ src/database/queries/deleteCohortStudent.js
@@ -1,9 +1,9 @@
 const dbConnection = require('../db_connection');
 
-const deleteStudentQuery = ({ stdId, cohortID }, cb) => {
+const deleteStudentQuery = ({ stdId, cohortId }, cb) => {
   const sql = {
     text: 'DELETE FROM student where id=$1 AND cohort_id=$2',
-    values: [stdId, cohortID],
+    values: [stdId, cohortId],
   };
   dbConnection.query(sql, (err, result) => {
     if (err) return cb(err);
