d1dc1edc190925241827400bf11749cfb9f63eff
ali <ali.7@outlook.com>
2018-09-18 23:28:35 +0300

    fix-issues relates to #19

--- src/database/queries/addProject.js
+++ src/database/queries/addProject.js
@@ -3,8 +3,8 @@ const dbConnection = require('../db_connection');
 const addProjectQuery = (newProject, cb) => {
   const sql = {
     text: 'INSERT INTO project (name, description, githublink, websitelink, img_url, cohort_id) values ($1,$2,$3,$4,$5,$6) RETURNING name',
-    values: [newProject.nameVal, newProject.descriptionVal, newProject.gitLinkVal,
-      newProject.webLinkVal, newProject.imgUrlVal, newProject.cohortId],
+    values: [newProject.nameVal, newProject.descriptionVal,
+      newProject.gitLinkVal, newProject.webLinkVal, newProject.imgUrlVal, newProject.cohortId],
   };
   dbConnection.query(sql, (err, result) => {
     if (err) return cb(err);
