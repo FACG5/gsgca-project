ec3755845a543b9e0993d204857ca72b2184923c
ali <ali.7@outlook.com>
2018-09-18 22:00:23 +0300

    add-front-end-fun-for-addProject relates to #10'

--- public/js/projects.js
+++ public/js/projects.js
@@ -11,6 +11,10 @@ const descriptionError = document.getElementById('descriptionError');
 const gitError = document.getElementById('gitError');
 const webError = document.getElementById('webError');
 const imgError = document.getElementById('imgError');
+const addProjectButton = document.getElementById('addproj');
+const URL = window.location.href;
+const splitUrl = URL.split('/');
+const cohortId = splitUrl[splitUrl.length - 2];
 
 const students = () => {
   student.classList.toggle('sectionstd--visible');
@@ -75,3 +79,38 @@ description.addEventListener('focusout', checkDescription);
 gitLink.addEventListener('focusout', checkGitLink);
 webLink.addEventListener('focusout', checkWebLink);
 imgUrl.addEventListener('focusout', checkImage);
+
+addProjectButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (checkName() && checkDescription() && checkGitLink() && checkWebLink() && checkImage()) {
+    const nameVal = name.value;
+    const descriptionVal = description.value;
+    const webLinkVal = webLink.value;
+    const gitLinkVal = gitLink.value;
+    const imgUrlVal = imgUrl.value;
+
+    const newProject = {
+      nameVal,
+      descriptionVal,
+      webLinkVal,
+      gitLinkVal,
+      imgUrlVal,
+      cohortId,
+    };
+
+    fetch(`/admin/community/${cohortId}/newProject`, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newProject),
+    })
+      .then((response) => { response.json(); })
+      .then((response) => {
+        swal('Good job!', 'Add Successfully!', 'success').then((value) => {
+          window.location = `/admin/community/${cohortId}/projects`;
+          JSON.stringify(response);
+        });
+      })
+      .catch(error => swal(error, 'error', 'error'));
+  }
+});
