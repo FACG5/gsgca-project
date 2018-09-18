f5bdcd4f352ecb53504e563f31b7ec057d0cb661
ali <ali.7@outlook.com>
2018-09-18 23:54:07 +0300

    create-edit-route-and-functions relates to #12

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,5 +1,7 @@
 const addProject = require('../database/queries/addProject');
 const deleteProject = require('../database/queries/deleteProject');
+const getSingleProject = require('../database/queries/getSingleProject');
+const editProject = require('../database/queries/editProject');
 
 exports.post = (request, response) => {
   addProject(request.body, (err, res) => {
@@ -12,5 +14,43 @@ exports.delete = (request, response) => {
   deleteProject(request.body, (err, res) => {
     if (err) return response.send(JSON.stringify({ err }));
     return response.send(JSON.stringify({ err: null, message: 'Project Deleted !' }));
+  });
+};
+
+exports.getProject = (request, response) => {
+  const { cohortId, projectId } = request.params;
+  getSingleProject({ cohortId, projectId }, (err, res) => {
+    if (err) {
+      return response.render('editProject', {
+        err: 'cannot get Project !',
+        styleFile: 'projects',
+        jsFile: 'editProject',
+        layout: 'adminLayout',
+        title: 'Admin Panel | Edit Projects',
+      });
+    }
+    if (res.length === 0) {
+      return response.render('editProject', {
+        err: 'There is no project by this ID !',
+        styleFile: 'projects',
+        jsFile: 'editProject',
+        layout: 'adminLayout',
+        title: 'Admin Panel | Edit Projects',
+      });
+    }
+    return response.render('editProject', {
+      res,
+      styleFile: 'projects',
+      jsFile: 'editProject',
+      layout: 'adminLayout',
+      title: 'Admin Panel | Edit Projects',
+    });
+  });
+};
+
+exports.edit = (request, response) => {
+  editProject(request.body, (err, res) => {
+    if (err) return response.send(JSON.stringify({ err }));
+    return response.send(JSON.stringify({ err: null, res }));
   });
 };
