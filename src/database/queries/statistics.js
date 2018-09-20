32d761d0b672befad6c48cdfd4bdd7ec6eb48cfe
ali <ali.7@outlook.com>
2018-09-20 11:26:18 +0300

    delete-statistics

--- src/database/queries/statistics.js
+++
@@ -1,3 +0,0 @@
-// const dbConnection = require('../db_connection');
-// select (select count(*) from cohort) as count_cohort, (select count(*) from project) as count_project, (select count(*) from student) as count_student
-
