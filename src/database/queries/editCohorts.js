655e0c2b00f9c8faa69424a79e5a519f4d540bda
lubnaabad <lubna_mosa@hotmail.com>
2018-09-22 19:26:48 +0300

    fix bug edit cohort relates #70

--- src/database/queries/editCohorts.js
+++ src/database/queries/editCohorts.js
@@ -2,12 +2,12 @@ const dbconnection = require('../dbConnection');
 
 const editCohorts = cohortData => new Promise((resolve, reject) => {
   const {
-    name, description, githublink, imgUrl, cohortId,
+    name, description, githubLink, imgUrl, cohortId,
   } = cohortData;
   
   const sql = {
     text: 'UPDATE cohort SET name = $1, description = $2, githublink = $3, img_url = $4 WHERE id=$5;',
-    values: [name, description, githublink, imgUrl, cohortId],
+    values: [name, description, githubLink, imgUrl, cohortId],
   };
   dbconnection.query(sql, (err, res) => {
     if (err) {
